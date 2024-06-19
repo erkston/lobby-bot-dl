09a1184c21d2b3651affd23ea61d15bfca97268d
erkston <erkston@gmail.com>
2024-06-19 15:52:08 -0400

    v0.3.20

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ BotGame = config['BotGame']
 BotAdminRole = config['BotAdminRole']
 
 
-version = "v0.3.19"
+version = "v0.3.20"
 Units = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
 utc = datetime.datetime.now(timezone.utc)
 Lobbies = []
